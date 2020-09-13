89f72325967cd44d49bd9d9f6b3d4460e29adb20
LucaFumagalli99 <lucafu99@gmail.com>
2020-09-14 01:51:58 +0200

    creazione login

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpCommunicationsService } from './HttpCommunications/http-communications.service';
 
 
 
 @NgModule({
   declarations: [],
+  providers: [HttpCommunicationsService],
   imports: [
     CommonModule
   ]
